d5745436c3ac60971571fc5be2714331ca9c61d8
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-02-28 22:34:31 +0100

    Updated Word List

--- src/wordList/Main.java
+++ src/wordList/Main.java
@@ -1,51 +1,67 @@
 package wordList;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.TreeMap;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    // gets words and frequency of occurence from a file
     static TreeMap<String, Integer> getWordsFromFile(String filePath) {
         File file = new File(filePath);
         TreeMap<String, Integer> wordMap = new TreeMap<>();
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = br.readLine()) != null) {
-                line = line.replaceAll("[,\\.\\?\\!]", " ").trim().toLowerCase();
+                // remove specific punctuation
+                line = line.replaceAll("[,.?!]", " ").trim().toLowerCase();
+                // increment frequency if word exists, set to 1 if it doesn't
                 for (String word: line.split("\\s+")) {
-                    if (wordMap.get(word) != null) {
-                        wordMap.put(word, wordMap.get(word) + 1);
-                    } else {
-                        wordMap.put(word, 1);
-                    }
+                    wordMap.merge(word, 1, Integer::sum);
                 }
             }
         } catch (FileNotFoundException e) {
-            System.out.println("File not found");
+            System.out.println("File not found " + filePath);
         } catch (IOException e) {
             System.out.println("Failed to read file");
         }
         return wordMap;
     }
 
-    // sort map by value
+    // sorts map by value in descending order
     static LinkedHashMap<String, Integer> sortMapByValue(Map<String, Integer> map) {
         LinkedHashMap<String, Integer> sortedMap = new LinkedHashMap<>();
         ArrayList<Integer> list = new ArrayList<>();
         for (Map.Entry<String, Integer> entry: map.entrySet()) {
-            list.add(null);
+            list.add(entry.getValue());
+        }
+        Collections.sort(list, Comparator.reverseOrder());
+        // put elements in sorted map in the order that they appear in the list
+        for (Integer num: list) {
+            for (Map.Entry<String, Integer> entry: map.entrySet()) {
+                if (entry.getValue().equals(num)) {
+                    sortedMap.put(entry.getKey(), num);
+                }
+            }
         }
         return sortedMap;
     }
 
+    // writes content of map to a file
+    static void writeMapToFile(Map<String, Integer> map, String filePath) {
+        File file = new File(filePath);
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+            // write word and frequency to file
+            for (String word: map.keySet()) {
+                bw.write(word + " " + map.get(word));
+                bw.newLine();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found " + filePath);
+        } catch (IOException e) {}
+    }
+
     public static void main(String[] args) {
-        TreeMap<String, Integer> wordMap = getWordsFromFile("./src/wordList/words.txt");
-        System.out.println(wordMap);
+        TreeMap<String, Integer> wordMap = getWordsFromFile("words.txt");
+        writeMapToFile(wordMap, "wordsListedAlphabetically.txt");
+        writeMapToFile(sortMapByValue(wordMap), "wordsSortedByFrequency.txt");
     }
 }
